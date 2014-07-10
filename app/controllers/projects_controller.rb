799b732a6292b12a0deff5774c46466095dc2346
Bill Siggelkow <bsiggelkow@mac.com>
2014-07-10 11:35:04 -0400

    Create views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Projects.create(name: params[:name])
+    @project = Project.create(name: params[:name])
     @projects = Project.all
   end
 
